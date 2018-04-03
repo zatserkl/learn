1b68f916751b6c5dec6f626536095cad73c56f5b
zatserkl <zatserkl@gmail.com>
2018-04-03 16:42:54 -0700

    converted to python3. Added comments

--- python/xe2.py
+++ python/xe2.py
@@ -1,9 +1,13 @@
 #! /usr/bin/env python
 
+""" Checks for unicode character for double quote character.
+It can appear in the proram code after copy/paste from pdf file.
+"""
+
 import sys
 
 if len(sys.argv) < 2:
-    print 'Usage:', sys.argv[0], 'file_to_check'
+    print('Usage:', sys.argv[0], 'file_to_check')
     exit()
 
 fname = sys.argv[1]
@@ -11,4 +15,4 @@ fname = sys.argv[1]
 with open(fname) as fp:
     for i, line in enumerate(fp):
         if "\xe2" in line:
-            print i, '\t', repr(line)
+            print(i, '\t', repr(line))
