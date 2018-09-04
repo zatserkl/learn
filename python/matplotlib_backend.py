bd05872dbb15c16105c8f2cc6582b2ce276564bf
zatserkl <zatserkl@gmail.com>
2018-09-03 17:50:41 -0700

    initial commit

---
+++ python/matplotlib_backend.py
@@ -0,0 +1,23 @@
+import matplotlib
+
+"""
+The statement
+
+matplotlib.use('Agg')
+
+should be used before import maptlotlib.pyplot
+otherwise, you will get a warning on it:
+
+UserWarning:
+This call to matplotlib.use() has no effect because the backend has already
+been chosen; matplotlib.use() must be called *before* pylab, matplotlib.pyplot,
+or matplotlib.backends is imported for the first time.
+"""
+matplotlib.use('Agg')  # PNG backend. Should be before import matplotlib.pyplot
+
+
+import matplotlib.pyplot as plt
+
+
+plt.plot([1, 2, 3])
+plt.savefig('matplotlib_backend')
