b6280cb226bbfcea4953d2d3ce377dbe8f664478
zatserkl <zatserkl@gmail.com>
2018-12-02 13:49:35 -0800

    initial commit

---
+++ python/test_order_fun.py
@@ -0,0 +1,18 @@
+# Andriy Zatsrklyaniy <zatserkl@gmail.com> Dec 2, 2018
+
+def main():
+    """ Call of function that defined later (below) in the module.
+    """
+    fun1()
+    fun2()
+
+
+def fun1():
+    print("fun1")
+
+
+def fun2():
+    print("fun2")
+
+
+main()
